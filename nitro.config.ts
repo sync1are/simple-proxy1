eee40e06b26efb76d9ff9ea15f85db01c04a985e
Synclare <goldencrusader2007@gmail.com>
2026-04-15 23:43:31 +0530

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,14 +1,18 @@
 import { join } from "path";
 import pkg from "./package.json";
 
-//https://nitro.unjs.io/config
+// https://nitro.unjs.io/config
 export default defineNitroConfig({
   noPublicDir: true,
+  serveStatic: false, // 🔥 THIS FIXES YOUR ERROR
+
   compatibilityDate: "2025-04-20",
   srcDir: "./src",
+
   runtimeConfig: {
     version: pkg.version
   },
+
   alias: {
     "@": join(__dirname, "src")
   }
